cd7b4cc6a9cad4e50b12efaa00b1232f2d557587
manishkumarroy <manishkumar222mani@gmail.com>
2022-04-17 23:48:34 +0530

    shift operation

--- singlyLinkList.js
+++ singlyLinkList.js
@@ -51,6 +51,18 @@ class SinglyLinkedList{
     return current;
 
   }
+
+shift(){
+  if(this.length===0) return undefined;
+
+  var currentHead = this.head;
+  this.head = currentHead.next;
+  this.length--;
+  if(this.length === 0)
+    this.tail = null;
+  
+    return currentHead;
+}
 }
 
 var list = new SinglyLinkedList();
