af86e0ef7f4e7088d485c333ab52171a2a4fb9ff
manishkumarroy <manishkumar222mani@gmail.com>
2022-04-21 00:11:13 +0530

    get set Insert function

--- singlyLinkList.js
+++ singlyLinkList.js
@@ -76,6 +76,52 @@ this.head = newNode;
 this.length++;
 return this;
 }
+ 
+get(idx){
+if(idx < 0 || idx >= this.length) return null;
+var counter = 0;
+var current = this.head;
+while(counter !== idx){
+  current = current.next;
+  counter++;
+}
+return current;
+}
+
+set(idx,value){
+foundNode = this.get(idx);
+if(foundNode){
+  foundNode.val = value;
+  return true;
+}
+return false;
+}
+
+Insert(idx,val){
+ if(idx < 0 || idx > this.lenth) return false;
+ if(idx === this.length){
+   this.push(val);
+   return true;
+ }
+ if(idx === 0){
+   this.unshift(val);
+   return true;
+ }
+ var newnode = new Node(val);
+ //we can use our get function for below code till while loop
+ // var prevnode = this.get(idx-1); 
+ var prevNode = this.head;
+ var count = 0;
+ while(count !== idx){
+   prevNode = prevNode.next;
+   count++;
+ }
+ var temp = prevNode.next;
+ prevNode.next = newNode;
+ newNode.next = temp;
+ this.length++;
+ return true;
+}
 }
 
 var list = new SinglyLinkedList();
